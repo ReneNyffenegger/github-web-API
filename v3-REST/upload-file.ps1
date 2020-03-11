b81b5bfcf59d4d870fb2e8c80d437986e2e594d9
René Nyffenegger <mail@renenyffenegger.ch>
2020-03-11 11:26:39 +0100

    + upload-file.ps1

---
+++ v3-REST/upload-file.ps1
@@ -0,0 +1,63 @@
+#
+#   Assign 'continue' to preference variable $verbosePreference
+#   for verbose output.
+#
+
+set-strictMode -version 2
+
+function upload-file-to-github-repo($localPath, $localFilename, $repoPath, $repoFilename, $repoName, $repoOwner, $message, $token) {
+   #
+   #      File content must be represented in base 64
+   #
+   $base64 = [System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes("$localPath/$localFilename"))
+
+   $url = "https://api.github.com/repos/$repoOwner/$repoName/contents/$repoPath/$repoFilename"
+
+   try {
+
+    #
+    #     TODO: PowerShell 7 introduces option -skipHTTPErrorCheck
+    #           This option should probably be used in order to have
+    #           see what HTTP status code was returned.
+    #
+
+    # github API token is required for private repositories
+
+      $response = invoke-restMethod                      `
+         $url                                            `
+        -headers @{Authorization = "bearer $token"}
+
+   }
+   catch [System.Net.WebException] {
+     #
+     #  I am too lazy to evaluate the details and just assume
+     #  that this is a 404. So the resource needs to be created
+     #
+        write-verbose 'Resource probably does not exist. Trying to create it'
+
+        $body = '{{"message": "{0}", "content": "{1}" }}' -f $message, $base64
+
+        $response = invoke-webrequest $url                                               `
+          -method          PUT                                                           `
+          -contentType     application/json                                              `
+          -headers       @{Authorization = "bearer $token"}                              `
+          -body            $body
+
+      #
+      # expected: 201 Created
+      #
+        write-verbose "$($response.StatusCode) $($response.StatusDescription)"
+        return
+   }
+   write-verbose "file seems to exit, updating it"
+   $sha = $response.sha
+   write-verbose "SHA = $sha"
+
+   $body = '{{"message": "{0}", "content": "{1}", "sha": "{2}" }}' -f $message, $base64, $sha
+
+   $response = invoke-webrequest $url                                               `
+     -method          PUT                                                           `
+     -contentType     application/json                                              `
+     -headers       @{Authorization = "bearer $token"}                              `
+     -body            $body
+}
